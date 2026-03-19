91a064671e4f22c2724fbec59fd416dd6c66f060
Sang Doan <std35@georgetown.edu>
2026-03-19 08:30:19 -0700

    Fix slide effect behavior and update tests

--- lib/src/effects/slide_effect.dart
+++ lib/src/effects/slide_effect.dart
@@ -7,7 +7,7 @@ import '../../motor_animate.dart';
 /// Defaults to `begin=Offset(0, -0.5), end=Offset.zero`
 /// (slide down from half its height).
 ///
-/// See also: [MoveEffect] to use pixel offsets.
+/// See also: [SlideAbsEffect] for pixel offsets.
 @immutable
 class SlideEffect extends Effect<Offset> {
   static const Offset neutralValue = Offset(neutralSlide, neutralSlide);
@@ -36,6 +36,53 @@ class SlideEffect extends Effect<Offset> {
     return SlideTransition(
       position: buildAnimation(controller, entry),
       child: child,
+    );
+  }
+}
+
+/// An effect that moves the target between the specified [begin] and [end] offsets
+/// in absolute pixels (via [Transform.translate]).
+/// Defaults to `begin=Offset(0, -16), end=Offset.zero`.
+@immutable
+class SlideAbsEffect extends Effect<Offset> {
+  static const Offset neutralValue = Offset(neutralSlide, neutralSlide);
+  static const Offset defaultValue = Offset(neutralSlide, defaultSlide);
+
+  static const double neutralSlide = 0.0;
+  static const double defaultSlide = -16.0;
+  static const bool defaultTransformHitTests = true;
+
+  const SlideAbsEffect({
+    super.delay,
+    super.motion,
+    Offset? begin,
+    Offset? end,
+    bool? transformHitTests,
+  })  : transformHitTests = transformHitTests ?? defaultTransformHitTests,
+        super(
+          begin: begin ?? (end == null ? defaultValue : neutralValue),
+          end: end ?? neutralValue,
+        );
+
+  final bool transformHitTests;
+
+  @override
+  Widget build(
+    BuildContext context,
+    Widget child,
+    AnimateController controller,
+    EffectEntry entry,
+  ) {
+    Animation<Offset> animation = buildAnimation(controller, entry);
+    return getOptimizedBuilder<Offset>(
+      animation: animation,
+      builder: (_, __) {
+        return Transform.translate(
+          offset: animation.value,
+          transformHitTests: transformHitTests,
+          child: child,
+        );
+      },
     );
   }
 }
@@ -95,6 +142,69 @@ extension SlideEffectExtensions<T extends AnimateManager<T>> on T {
         motion: motion,
         begin: SlideEffect.neutralValue.copyWith(dy: begin),
         end: SlideEffect.neutralValue.copyWith(dy: end),
+      ),
+    );
+  }
+
+  /// Adds a [SlideAbsEffect] that moves the target between the specified [begin]
+  /// and [end] absolute offsets (via [Transform.translate]).
+  T slideAbs({
+    Duration? delay,
+    Motion? motion,
+    Offset? begin,
+    Offset? end,
+    bool? transformHitTests,
+  }) =>
+      addEffect(
+        SlideAbsEffect(
+          delay: delay,
+          motion: motion,
+          begin: begin,
+          end: end,
+          transformHitTests: transformHitTests,
+        ),
+      );
+
+  /// Adds a [SlideAbsEffect] that moves the target horizontally by absolute pixel
+  /// values (via [Transform.translate]).
+  T slideXAbs({
+    Duration? delay,
+    Motion? motion,
+    double? begin,
+    double? end,
+    bool? transformHitTests,
+  }) {
+    begin ??= end == null ? SlideAbsEffect.defaultSlide : SlideAbsEffect.neutralSlide;
+    end ??= SlideAbsEffect.neutralSlide;
+    return addEffect(
+      SlideAbsEffect(
+        delay: delay,
+        motion: motion,
+        begin: SlideAbsEffect.neutralValue.copyWith(dx: begin),
+        end: SlideAbsEffect.neutralValue.copyWith(dx: end),
+        transformHitTests: transformHitTests,
+      ),
+    );
+  }
+
+  /// Adds a [SlideAbsEffect] that moves the target vertically by absolute pixel
+  /// values (via [Transform.translate]).
+  T slideYAbs({
+    Duration? delay,
+    Motion? motion,
+    double? begin,
+    double? end,
+    bool? transformHitTests,
+  }) {
+    begin ??= end == null ? SlideAbsEffect.defaultSlide : SlideAbsEffect.neutralSlide;
+    end ??= SlideAbsEffect.neutralSlide;
+    return addEffect(
+      SlideAbsEffect(
+        delay: delay,
+        motion: motion,
+        begin: SlideAbsEffect.neutralValue.copyWith(dy: begin),
+        end: SlideAbsEffect.neutralValue.copyWith(dy: end),
+        transformHitTests: transformHitTests,
       ),
     );
   }
--- test/effects/slide_test.dart
+++ test/effects/slide_test.dart
@@ -38,6 +38,39 @@ void main() {
     await tester.pumpAnimation(animation, initialDelay: 500.ms);
     _verifySlide(tester, 0, _slideAmt / 2);
   });
+
+  testWidgets('SlideEffect: slideAbs', (tester) async {
+    final animation = const FlutterLogo().animate().slideAbs(
+          motion: Motion.linear(1000.ms),
+          end: const Offset(_absSlideAmt, _absSlideAmt),
+        );
+
+    // check halfway
+    await tester.pumpAnimation(animation, initialDelay: 500.ms);
+    _verifySlideAbs(tester, _absSlideAmt / 2, _absSlideAmt / 2);
+  });
+
+  testWidgets('SlideEffect: slideXAbs', (tester) async {
+    final animation = const FlutterLogo().animate().slideXAbs(
+          motion: Motion.linear(1000.ms),
+          end: _absSlideAmt,
+        );
+
+    // check halfway
+    await tester.pumpAnimation(animation, initialDelay: 500.ms);
+    _verifySlideAbs(tester, _absSlideAmt / 2, 0);
+  });
+
+  testWidgets('SlideEffect: slideYAbs', (tester) async {
+    final animation = const FlutterLogo().animate().slideYAbs(
+          motion: Motion.linear(1000.ms),
+          end: _absSlideAmt,
+        );
+
+    // check halfway
+    await tester.pumpAnimation(animation, initialDelay: 500.ms);
+    _verifySlideAbs(tester, 0, _absSlideAmt / 2);
+  });
 }
 
 Future<void> _verifySlide(WidgetTester tester, double x, double y) async {
@@ -53,4 +86,18 @@ Future<void> _verifySlide(WidgetTester tester, double x, double y) async {
   );
 }
 
+Future<void> _verifySlideAbs(WidgetTester tester, double x, double y) async {
+  tester.expectWidgetWithDouble<Transform>(
+    (o) => o.transform.getTranslation().x,
+    x,
+    'x',
+  );
+  tester.expectWidgetWithDouble<Transform>(
+    (o) => o.transform.getTranslation().y,
+    y,
+    'y',
+  );
+}
+
 const double _slideAmt = 1;
+const double _absSlideAmt = 100;
